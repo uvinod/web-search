47667bafc9019a92c94d6197afc675b41da16f47
VINOD <vinodkumar.u93@gmail.com>
2017-04-09 14:27:08 +0800

    Rename Controllers

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class search extends CI_Controller {
+class Search extends CI_Controller {
 
 	function __construct() {
 		parent::__construct();		
