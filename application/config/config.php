af355a18bdb913e4a69198687a6e6fbba771bdf8
VINOD <vinodkumar.u93@gmail.com>
2017-04-09 14:24:15 +0800

    Update base URL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/login-search/';
+$config['base_url'] = 'https://websearchapp1.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
