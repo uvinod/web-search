745e79dbd3379632f088f954362ca60492654cf1
VINOD <vinodkumar.u93@gmail.com>
2017-04-09 15:19:57 +0800

    Troubleshooting

--- system/core/CodeIgniter.php
+++ system/core/CodeIgniter.php
@@ -402,7 +402,7 @@ if ( ! is_php('5.4'))
 	$e404 = FALSE;
 	$class = ucfirst($RTR->class);	
 	$method = $RTR->method;
-
+	echo $class;die;
 	
 	if (empty($class) OR ! file_exists(APPPATH.'controllers/'.$RTR->directory.$class.'.php'))
 	{
