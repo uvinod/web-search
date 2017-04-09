a6b07dfffae735822674e52f89532e4121412fe8
VINOD <vinodkumar.u93@gmail.com>
2017-04-09 14:36:20 +0800

    Troubleshooting

--- index.php
+++ index.php
@@ -63,6 +63,9 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
+
+
+echo "testtest";die;
 switch (ENVIRONMENT)
 {
 	case 'development':
